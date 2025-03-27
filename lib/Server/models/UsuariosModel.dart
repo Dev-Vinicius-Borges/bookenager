712497424e4dea35a654a0c2d753155e0ee9ea14
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2025-03-27 18:41:55 -0300

    Feat: Criação do modelo de usuários.

---
+++ lib/Server/models/UsuariosModel.dart
@@ -0,0 +1,9 @@
+class UsuariosModel {
+  final int id;
+  final String nome;
+  final String email;
+  final String senha;
+
+  UsuariosModel({required this.id, required this.nome, required this.email, required this.senha});
+
+}
