e2fa2067c6b32dc6aa8ba281aba3da28793c261e
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2025-03-27 18:39:20 -0300

    Feat: Criação de DTOS para representar a criação e login de usuários.

---
+++ lib/Server/dtos/Usuario/CriarUsuarioDto.dart
@@ -0,0 +1,7 @@
+class CriarUsuarioDto{
+  final String nome;
+  final String email;
+  final String senha;
+
+  CriarUsuarioDto({required this.nome, required this.email, required this.senha});
+}
---
+++ lib/Server/dtos/Usuario/FazerLoginDto.dart
@@ -0,0 +1,6 @@
+class FazerLoginDto{
+  final String email;
+  final String senha;
+
+  FazerLoginDto({required this.email, required this.senha});
+}
