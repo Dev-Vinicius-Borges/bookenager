ede3ab5716b96c35c3c1b6b868d895c4ca9506cd
dev-vinicius-borges <dev.vinicius.borges@outlook.com>
2025-04-17 17:25:06 -0300

    Feat: Adicionando novas informações.

--- lib/Server/dtos/livro/CriarLivroDto.dart
+++ lib/Server/dtos/livro/CriarLivroDto.dart
@@ -3,11 +3,13 @@ class CriarLivroDto {
   final String autor;
   final int paginas_lidas;
   final int id_usuario;
+  final int genero;
 
   CriarLivroDto({
     required this.titulo,
     required this.autor,
     required this.paginas_lidas,
     required this.id_usuario,
+    required this.genero
   });
 }
--- lib/Server/models/LivrosModel.dart
+++ lib/Server/models/LivrosModel.dart
@@ -1,13 +1,17 @@
 class LivrosModel {
+  final int? id_livro;
   final String titulo;
   final String autor;
   final int paginas_lidas;
   final int id_usuario;
+  final int genero;
 
   LivrosModel({
+    this.id_livro,
     required this.titulo,
     required this.autor,
     required this.paginas_lidas,
     required this.id_usuario,
+    required this.genero
   });
 }
